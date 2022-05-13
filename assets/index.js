60bbe63a9930076d444cf25c812f982f8ebdc826
anaismiraira <mirairaanais@gmail.com>
2022-05-13 18:21:11 -0400

    1820 13052022 banner-desktop

--- assets/index.js
+++ assets/index.js
@@ -14,3 +14,18 @@ new WOW({offset:offset, scrollContainer: null}).init()
 
 $('header').load('components/header.html')
 $('footer').load('components/footer.html')
+
+// ------------------------------Banner-----------------------------
+const swiperbanner = new Swiper(".swiper-banner", {
+  spaceBetween: 4,
+  loop: true,
+  slidesPerView: 1,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
