717557d3f8da9c2bacbe2752f797b3a261a61e10
anaismiraira <mirairaanais@gmail.com>
2022-05-23 11:20:31 -0400

    1120 23052022 form tickets

--- assets/index.js
+++ assets/index.js
@@ -84,6 +84,65 @@ if (screen.width < 768) {
         },
       })
   }
+//---------------------------------Tckets-------------------------------
+function dataSubmite(data) {
+  const requestOptions = {
+    method: 'POST',
+    body: data,
+    headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+    },
+  };
+  fetch("https://www.infocasas.com.uy/proyectos/osaka?&formulario=1&json=1", requestOptions)
+  .then((json) => {
+    setTimeout(()=>{
+      if (json.status === 200) {
+        $('#formSuccess').fadeIn();
+      } else {
+        $('#formError').fadeIn();
+      }
+      $('#formSending').hide();
+    }, 2000)
+  })
+  .catch(error => {
+    console.log('error', error);
+    setTimeout(() => {
+      $('#formSending').hide();
+      $('#formError').fadeIn();
+    }, 2000)
+  });
+}
+
+function submite() {
+ 'use strict'
+  const form = document.querySelector('#ticketsForm')
+  const data = JSON.stringify({
+    nombre: form.name.value,
+    email: form.email.value,
+    telefono: form.phone.value,
+    tel: form.phone.value,
+    cantidad: form.cantidad.value,
+    seleccion: form.seleccion.value,
+    source: 2,
+    utm_source: "web_cliente",
+    utm_medium: "austin",
+    InfoLeads: 1,
+    IDflow_execution: 4315
+  })
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }else{
+    dataSubmite(data)
+    setTimeout(()=>{
+      $(form).fadeOut();
+      $('#formSending').fadeIn();
+    },300)
+  }
+  form.classList.add('was-validated')
+}
+
+
 //---------------------------------Form-------------------------------
 function dataSubmited(data) {
   const requestOptions = {
